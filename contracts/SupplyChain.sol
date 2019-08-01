3540364e9badc4122d9af749046cf510eb8f54c0
System Administrator <root@Shas-MacBook-Pro.local>
2019-07-31 20:14:29 -0400

    supply chain

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -1,10 +1,6 @@
-/*
-    This exercise has been updated to use Solidity version 0.5
-    Breaking changes from 0.4 to 0.5 can be found here: 
-    https://solidity.readthedocs.io/en/v0.5.0/050-breaking-changes.html
-*/
 
 pragma solidity ^0.5.0;
+
 
 contract SupplyChain {
 
@@ -29,12 +25,10 @@ contract SupplyChain {
     Here, add a name, sku, price, state, seller, and buyer
     We've left you to figure out what the appropriate types are,
     if you need help you can ask around :)
-    Be sure to add "payable" to addresses that will be handling value transfer
   */
 
   /* Create 4 events with the same name as each possible State (see above)
-    Prefix each event with "Log" for clarity, so the forSale event will be called "LogForSale"
-    Each event should accept one argument, the sku */
+    Each event should accept one argument, the sku*/
 
 /* Create a modifer that checks if the msg.sender is the owner of the contract */
 
@@ -51,11 +45,7 @@ contract SupplyChain {
 
   /* For each of the following modifiers, use what you learned about modifiers
    to give them functionality. For example, the forSale modifier should require
-   that the item with the given sku has the state ForSale. 
-   Note that the uninitialized Item.State is 0, which is also the index of the ForSale value,
-   so checking that Item.State == ForSale is not sufficient to check that an Item is for sale.
-   Hint: What item properties will be non-zero when an Item has been added?
-   */
+   that the item with the given sku has the state ForSale. */
   modifier forSale
   modifier sold
   modifier shipped
@@ -67,11 +57,10 @@ contract SupplyChain {
        and set your skuCount to 0. */
   }
 
-  function addItem(string memory _name, uint _price) public returns(bool){
-    emit LogForSale(skuCount);
-    items[skuCount] = Item({name: _name, sku: skuCount, price: _price, state: State.ForSale, seller: msg.sender, buyer: address(0)});
+  function addItem(string _name, uint _price) public {
+    emit ForSale(skuCount);
+    items[skuCount] = Item({name: _name, sku: skuCount, price: _price, state: State.ForSale, seller: msg.sender, buyer: 0});
     skuCount = skuCount + 1;
-    return true;
   }
 
   /* Add a keyword so the function can be paid. This function should transfer money
@@ -97,7 +86,7 @@ contract SupplyChain {
   {}
 
   /* We have these functions completed so we can run tests, just ignore it :) */
-  function fetchItem(uint _sku) public view returns (string memory name, uint sku, uint price, uint state, address seller, address buyer) {
+  function fetchItem(uint _sku) public view returns (string name, uint sku, uint price, uint state, address seller, address buyer) {
     name = items[_sku].name;
     sku = items[_sku].sku;
     price = items[_sku].price;
@@ -108,3 +97,4 @@ contract SupplyChain {
   }
 
 }
+
